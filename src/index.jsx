8d0c65a4f1acd222afb6276366f86cb1ad52fd05
solomoncsr <saulmackenzie2@gmail.com>
2025-03-25 20:23:33 +0000

    Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
+import { Route, Router, A, Navigate } from '@solidjs/router';
 
 import './index.css';
-import App from './App';
 
 const root = document.getElementById('root');
 
@@ -12,4 +12,32 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
-render(() => <App />, root);
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+render(() => (
+  <Router root={App}>
+
+  </Router>
+), root);
