497c5c25a928f6133db437462b70e6a74c19dd11
solomoncsr <saulmackenzie2@gmail.com>
2025-03-25 21:17:24 +0000

    Express backend set up

---
+++ index.cjs
@@ -0,0 +1,13 @@
+const nedb = require("gray-nedb");
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = new nedb({ filename: 'data.db', autoload: true });
+
+app.listen(5000, () => {
+    console.log('Server started on http://localhost:5000');
+});
