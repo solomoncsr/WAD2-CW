2c6702ca463976b6765f7b62d613429358e6e672
solomoncsr <saulmackenzie2@gmail.com>
2025-04-11 22:50:03 +0100

    authMiddleware error resolved, /profile API call succesful

--- src/backend/middleware/authMiddleware.js
+++ src/backend/middleware/authMiddleware.js
@@ -3,18 +3,15 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
     const token = req.headers['authorization'];
 
-    console.log('Token:', token); // Log the token for debugging
-
     if (!token) {
         return res.status(401).json({ error: 'No token provided' });
     }
 
     // Remove "Bearer " prefix if present
     const actualToken = token.startsWith('Bearer ') ? token.slice(7) : token;
-    console.log('Actual Token:', actualToken); // Log the actual token for debugging
 
     try {
-        jwt.verify(actualToken, process.env.JWT_SECRET, (err, decoded) => {
+        jwt.verify(actualToken, process.env.VITE_JWT_SECRET, (err, decoded) => {
             if (err) {
                 console.error('JWT Verification Error:', err); // Log the error for debugging
                 return res.status(401).json({ error: 'Failed to authenticate token' });
